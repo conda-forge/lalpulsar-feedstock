f58faf86829822ca5b569db0a416c5a9ea0903c2
Duncan Macleod <duncan.macleod@ligo.org>
2021-02-02 16:23:20 +0000

    fix activate script

--- recipe/install-lib.sh
+++ recipe/install-lib.sh
@@ -15,25 +15,23 @@ make -j ${CPU_COUNT} V=1 VERBOSE=1 install-pkgconfigDATA
 
 # -- create activate/deactivate scripts
 
-PKG_NAME_UPPER=$(echo ${PKG_NAME} | awk '{ print toupper($0) }')
-
 # activate.sh
-ACTIVATE_SH="${PREFIX}/etc/conda/activate.d/activate_${PKG_NAME}.sh"
+ACTIVATE_SH="${PREFIX}/etc/conda/activate.d/activate_lalpulsar.sh"
 mkdir -p $(dirname ${ACTIVATE_SH})
 cat > ${ACTIVATE_SH} << EOF
 #!/bin/bash
-export CONDA_BACKUP_${PKG_NAME_UPPER}_DATADIR="\${${PKG_NAME_UPPER}_DATADIR:-empty}"
-export ${PKG_NAME_UPPER}_DATADIR="/opt/anaconda1anaconda2anaconda3/share/${PKG_NAME}"
+export CONDA_BACKUP_LALPULSAR_DATADIR="\${LALPULSAR_DATADIR:-empty}"
+export LALPULSAR_DATADIR="/opt/anaconda1anaconda2anaconda3/share/lalpulsar"
 EOF
 # deactivate.sh
-DEACTIVATE_SH="${PREFIX}/etc/conda/deactivate.d/deactivate_${PKG_NAME}.sh"
+DEACTIVATE_SH="${PREFIX}/etc/conda/deactivate.d/deactivate_lalpulsar.sh"
 mkdir -p $(dirname ${DEACTIVATE_SH})
 cat > ${DEACTIVATE_SH} << EOF
 #!/bin/bash
-if [ "\${CONDA_BACKUP_${PKG_NAME_UPPER}_DATADIR}" = "empty" ]; then
-	unset ${PKG_NAME_UPPER}_DATADIR
+if [ "\${CONDA_BACKUP_LALPULSAR_DATADIR}" = "empty" ]; then
+	unset LALPULSAR_DATADIR
 else
-	export ${PKG_NAME_UPPER}_DATADIR="\${CONDA_BACKUP_${PKG_NAME_UPPER}_DATADIR}"
+	export LALPULSAR_DATADIR="\${CONDA_BACKUP_LALPULSAR_DATADIR}"
 fi
-unset CONDA_BACKUP_${PKG_NAME_UPPER}_DATADIR
+unset CONDA_BACKUP_LALPULSAR_DATADIR
 EOF
