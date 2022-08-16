6ef42fcf7e413b071b45ad6d1dcad293b81afb65
duncan.macleod <duncan.macleod@ligo.org>
2022-08-16 10:57:56 +0100

    update recipe
    many scripts moved from lalapps
    now a requirement on lalinference

--- recipe/install-bin.sh
+++ recipe/install-bin.sh
@@ -2,12 +2,17 @@
 
 set -ex
 
+_make="make -j ${CPU_COUNT} V=1 VERBOSE=1"
+
 # install from python build directory
 _pybuilddir="_build${PY_VER}"
 cd ${_pybuilddir}
 
+# test binaries
+${_make} -C bin check
+
 # install binaries
-make -j ${CPU_COUNT} V=1 VERBOSE=1 install -C bin
+${_make} -C bin install
 
 # install system configuration files
-make -j ${CPU_COUNT} V=1 VERBOSE=1 install-sysconfDATA
+${_make} install-sysconfDATA
